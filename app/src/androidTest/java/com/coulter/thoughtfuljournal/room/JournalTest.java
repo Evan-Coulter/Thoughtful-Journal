75535eb857167bcf3795e6ef178a590cfa341311
Evan-Coulter <65514319+evan-coulter@users.noreply.github.com>
2021-07-12 22:58:20 -0700

    Add Journal Class Testing,
    Fixed DB test and added unit tests on Journal Class public methods.

--- app/src/androidTest/java/com/coulter/thoughtfuljournal/room/JournalTest.java
+++ app/src/androidTest/java/com/coulter/thoughtfuljournal/room/JournalTest.java
@@ -1,9 +1,73 @@
 package com.coulter.thoughtfuljournal.room;
 
-import androidx.test.ext.junit.runners.AndroidJUnit4;;
+import android.util.Log;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 @RunWith(AndroidJUnit4.class)
 public class JournalTest {
+    private static final String name = "Name";
+    private static final String content = "Content";
+    private static Date date;
+    static {
+        try {
+            date = new SimpleDateFormat("dd/MM/yyyy").parse("25/12/2021");
+        } catch (ParseException exception) {
+            exception.printStackTrace();
+        }
+    }
 
+    public void testEquals() {
+        Journal journal1 = new Journal(name, content, date);
+        Journal journal2 = new Journal(name, content, date);
+        assertEquals(journal1, journal2);
+    }
+
+    public void testNotEquals() {
+        Journal journal1 = new Journal(name, content, date);
+        Journal journal2 = new Journal(content, name, date);
+        assertNotEquals(journal1, journal2);
+    }
+
+    @Test
+    public void testConstructor() {
+        int size = 2;
+        String[] names = {"", "Name"};
+        String[] content = {"", "Lorem ipsum, or lipsum as it is sometimes known, " +
+                "is dummy text used in laying out print, graphic or web designs."};
+        String[] stringDates = {"00/00/0000", "25/12/2021"};
+        Date[] dates = {new Date(),new Date()};
+        for(int i=0; i<size; i++) {
+            try {
+                dates[i] = new SimpleDateFormat("dd/MM/yyyy").parse(stringDates[i]);
+            } catch(ParseException exception) {
+                dates[i] = null;
+            }
+        }
+        for(int i=0; i<size; i++) {
+            try {
+                Journal journal = new Journal(names[i], content[i], dates[i]);
+                if (!journal.journal_name.equals(names[i]) ||
+                        !journal.journal_content.equals(content[i]) ||
+                        !journal.creation_date.equals(dates[i])){
+                    fail();
+                }
+            } catch (Exception exception) {
+                Log.e("Constructor Test Failed", exception.toString());
+                fail();
+            }
+        }
+        assertTrue(true);
+    }
 }
--- app/src/main/java/com/coulter/thoughtfuljournal/room/Journal.java
+++ app/src/main/java/com/coulter/thoughtfuljournal/room/Journal.java
@@ -1,9 +1,16 @@
 package com.coulter.thoughtfuljournal.room;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 
@@ -21,29 +28,37 @@ public class Journal {
     @ColumnInfo(name="creation_date")
     public Date creation_date;
 
-    public Journal(String journal_name, String journal_content, Date creation_date) {
-        this.primaryKey = primaryKey;
+    public Journal(@NonNull String journal_name, @NonNull String journal_content, Date creation_date) {
         this.journal_name = journal_name;
         this.journal_content = journal_content;
-        this.creation_date = creation_date;
+        if(creation_date == null) {
+            this.creation_date = Calendar.getInstance().getTime();
+        } else{
+            this.creation_date = creation_date;
+        }
     }
 
     public boolean equals(Journal journal) {
         try {
-            return journal_name.equals(journal.journal_name)
-                    && journal_content.equals(journal.journal_content)
-                    && creation_date == journal.creation_date;
+            if(journal_name.equals(journal.journal_name) &&
+                journal_content.equals(journal.journal_content) &&
+                creation_date.equals(journal.creation_date)) {
+                return true;
+            }
         }catch (Exception exception) {
             exception.printStackTrace();
             return false;
         }
+        return false;
     }
 
+    @NotNull
     @Override
     public String toString() {
         return "Journal{" +
                 "primaryKey=" + primaryKey +
                 ", journal_name='" + journal_name + '\'' +
+                ", journal_content='" + journal_content + '\'' +
                 ", creation_date=" + creation_date +
                 '}';
     }
