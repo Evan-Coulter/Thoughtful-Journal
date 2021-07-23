9358718ba5ede01ed056108053f2327c2133ca5a
Evan-Coulter <65514319+evan-coulter@users.noreply.github.com>
2021-07-22 23:11:52 -0700

    Add Bold Formatting and Format Button Toggle Group,
    Added the first formatting on the bold button and included a toggle group to better interact with the formatting buttons.

--- app/src/main/java/com/coulter/thoughtfuljournal/fragments/EditJournalFragment.java
+++ app/src/main/java/com/coulter/thoughtfuljournal/fragments/EditJournalFragment.java
@@ -1,9 +1,15 @@
 package com.coulter.thoughtfuljournal.fragments;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.style.StyleSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
@@ -30,6 +36,22 @@ public class EditJournalFragment extends Fragment {
         EditJournalFragmentBinding binding = DataBindingUtil.inflate(inflater,R.layout.edit_journal_fragment,container,false);
         viewModel = new ViewModelProvider(requireActivity()).get(JournalViewModel.class);
         binding.setViewmodel(viewModel);
+        setupButtons(binding);
         return binding.getRoot();
+    }
+
+    private void setupButtons(EditJournalFragmentBinding binding) {
+        setupBoldButton(binding);
+    }
+
+    private void setupBoldButton(EditJournalFragmentBinding binding) {
+        binding.boldButton.setOnClickListener(view->{
+            Spannable string = new SpannableStringBuilder(binding.editText.getText());
+            string.setSpan(new StyleSpan(Typeface.BOLD),
+                    binding.editText.getSelectionStart(),
+                    binding.editText.getSelectionEnd(),
+                    0);
+            binding.editText.setText(string);
+        });
     }
 }
