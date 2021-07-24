465f1f14e54264184d38700c33b4a86a3b0e2cba
Evan-Coulter <65514319+evan-coulter@users.noreply.github.com>
2021-07-23 23:34:59 -0700

    Finish Basic Text Formatting,
    Finished the rest of the formatting buttons. Still need to implement method to handle partially formatted words, undoing formatting, saving formatting to room db.

--- app/src/main/java/com/coulter/thoughtfuljournal/fragments/EditJournalFragment.java
+++ app/src/main/java/com/coulter/thoughtfuljournal/fragments/EditJournalFragment.java
@@ -2,14 +2,16 @@ package com.coulter.thoughtfuljournal.fragments;
 
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.ParcelableSpan;
 import android.text.Spannable;
-import android.text.SpannableString;
 import android.text.SpannableStringBuilder;
+import android.text.style.AbsoluteSizeSpan;
 import android.text.style.StyleSpan;
+import android.text.style.UnderlineSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
+import android.widget.Button;
 
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
@@ -23,6 +25,7 @@ import org.jetbrains.annotations.NotNull;
 
 public class EditJournalFragment extends Fragment {
     public JournalViewModel viewModel;
+    private EditJournalFragmentBinding binding;
 
     public EditJournalFragment(){}
 
@@ -33,7 +36,7 @@ public class EditJournalFragment extends Fragment {
 
     @Override
     public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        EditJournalFragmentBinding binding = DataBindingUtil.inflate(inflater,R.layout.edit_journal_fragment,container,false);
+        binding = DataBindingUtil.inflate(inflater,R.layout.edit_journal_fragment,container,false);
         viewModel = new ViewModelProvider(requireActivity()).get(JournalViewModel.class);
         binding.setViewmodel(viewModel);
         setupButtons(binding);
@@ -41,17 +44,22 @@ public class EditJournalFragment extends Fragment {
     }
 
     private void setupButtons(EditJournalFragmentBinding binding) {
-        setupBoldButton(binding);
+        setupFormatButton(new StyleSpan(Typeface.BOLD), binding.boldButton, Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
+        setupFormatButton(new StyleSpan(Typeface.ITALIC), binding.italicButton, Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
+        setupFormatButton(new UnderlineSpan(), binding.underlineButton, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        setupFormatButton(new AbsoluteSizeSpan(22, true), binding.sizeButton, Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
     }
 
-    private void setupBoldButton(EditJournalFragmentBinding binding) {
-        binding.boldButton.setOnClickListener(view->{
+    private void setupFormatButton(ParcelableSpan span, Button button, int spanBounds) {
+        button.setOnClickListener(view->{
+            int selectionIndex = binding.editText.getSelectionEnd();
             Spannable string = new SpannableStringBuilder(binding.editText.getText());
-            string.setSpan(new StyleSpan(Typeface.BOLD),
+            string.setSpan(span,
                     binding.editText.getSelectionStart(),
                     binding.editText.getSelectionEnd(),
-                    0);
+                    spanBounds);
             binding.editText.setText(string);
+            binding.editText.setSelection(selectionIndex);
         });
     }
 }
