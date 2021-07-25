9cef32184aca701a7f6727f094cba61f6da27820
Evan-Coulter <65514319+evan-coulter@users.noreply.github.com>
2021-07-24 22:58:44 -0700

    Add Format Button Error Message On Incorrect Usage.

--- app/src/main/java/com/coulter/thoughtfuljournal/fragments/EditJournalFragment.java
+++ app/src/main/java/com/coulter/thoughtfuljournal/fragments/EditJournalFragment.java
@@ -21,12 +21,9 @@ import com.coulter.thoughtfuljournal.R;
 import com.coulter.thoughtfuljournal.databinding.EditJournalFragmentBinding;
 import com.coulter.thoughtfuljournal.viewmodel.JournalViewModel;
 import com.google.android.material.button.MaterialButton;
-import com.google.android.material.button.MaterialButtonToggleGroup;
+import com.google.android.material.snackbar.Snackbar;
 
 import org.jetbrains.annotations.NotNull;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class EditJournalFragment extends Fragment {
     public JournalViewModel viewModel;
@@ -55,28 +52,25 @@ public class EditJournalFragment extends Fragment {
         setupFormatButton(new AbsoluteSizeSpan(22, true), binding.sizeButton);
     }
 
-    private void setupFormatButton(ParcelableSpan span, Button button) {
+    private void setupFormatButton(ParcelableSpan span, MaterialButton button) {
         button.setOnClickListener(view-> {
-            //if there is text selected
             if (binding.editText.getSelectionStart() - binding.editText.getSelectionEnd() != 0) {
-                //only apply formatting
-                applyFormatting(span, button, binding.editText.getSelectionStart(), binding.editText.getSelectionEnd());
+                applyFormatting(span, binding.editText.getSelectionStart(), binding.editText.getSelectionEnd());
             } else {
-                displayError();
+                displayError(button);
             }
+            button.setChecked(false);
         });
     }
 
-    private void applyFormatting(ParcelableSpan span, Button button, int start, int end){
-        int selectionIndex = binding.editText.getSelectionEnd();
+    private void applyFormatting(ParcelableSpan span, int start, int end){
         Spannable string = new SpannableStringBuilder(binding.editText.getText());
         string.setSpan(span, start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
         binding.editText.setText(string);
-        binding.editText.setSelection(selectionIndex);
-        ((MaterialButton)button).setChecked(false);
+        binding.editText.setSelection(start, end);
     }
 
-    private void displayError() {
-        //show snack bar error telling user to highlight text first.
+    private void displayError(Button button) {
+        Snackbar.make(button, R.string.format_string_error, Snackbar.LENGTH_SHORT).show();
     }
 }
