fe90ad29537f95a0f81bb8133aba0c00fe1b0993
Evan-Coulter <65514319+evan-coulter@users.noreply.github.com>
2021-08-11 13:15:41 -0700

    Begin Immersive Reader Feature.

---
+++ app/src/main/java/com/coulter/thoughtfuljournal/fragments/ReadJournalFragment.java
@@ -0,0 +1,23 @@
+package com.coulter.thoughtfuljournal.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.coulter.thoughtfuljournal.databinding.ReadJournalFragmentBinding;
+
+
+public class ReadJournalFragment extends Fragment {
+    private ReadJournalFragmentBinding binding;
+
+    public ReadJournalFragment(){}
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return null;
+    }
+}
